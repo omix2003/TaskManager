05c75931eefd382cf6392596962263a5a4f7890a
Omkar Binage <99659981+omix2003@users.noreply.github.com>
2025-01-22 22:52:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://taskmanager-v8m1.onrender.com"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
